010257590e75b84b43fffe70bdd6814b4d53b3ff
Wang, Ming Derek <Ming.Derek.Wang@dnvgl.com>
2018-12-29 12:36:16 +0100

    add sample

---
+++ Sample/Demo/Demo/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    using Microsoft.Owin.Security;
+    using Microsoft.Owin.Security.OpenIdConnect;
+
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        [HttpGet]
+        public void SignOut()
+        {
+            if (Request.IsAuthenticated)
+            {
+                IEnumerable<AuthenticationDescription> authTypes = this.HttpContext.GetOwinContext().Authentication.GetAuthenticationTypes();
+                this.HttpContext.GetOwinContext().Authentication.SignOut(authTypes.Select(t => t.AuthenticationType).ToArray());
+                Request.GetOwinContext().Authentication.GetAuthenticationTypes();
+            }
+        }
+    }
+}
---
+++ Sample/Demo/Demo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Veracity.Authentication.OpenIDConnect.AspNet;
+
+    public class HomeController : Controller
+    {
+        private static HttpClient client;
+
+        public HomeController()
+        {
+            client = new HttpClient();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<ActionResult> CallApiAsync()
+        {
+            var service = new VeracityPlatformService(client, this.HttpContext);
+            var request = new HttpRequestMessage(HttpMethod.Get, "/platform/my/profile");
+            request.Headers.Authorization = await service.GetAuthenticationHeaderAsync();
+            var response = await client.SendAsync(request);
+            ViewData["Payload"] = await response.Content.ReadAsStringAsync();
+            return View();
+        }
+    }
+}
